819f85bd34ed5815d8d92c2940b14d392244c889
Alexthian19 <ge19008@ues.edu.sv>
2023-06-14 22:49:12 -0600

    insertar la barra de busqueda por fecha en nominas

--- src/app/gestion-nominas/nomina-mensual/page.jsx
+++ src/app/gestion-nominas/nomina-mensual/page.jsx
@@ -1,27 +1,66 @@
 "use client";
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import { useState } from "react";
+import {
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Flex,
+  Input,
+  Text,
+  Button,
+  Spinner,
+} from "@chakra-ui/react";
 import NominaEmpleados from "@app/components/NominaEmpleados";
 import NominaEmpresa from "@app/components/NominaEmpresa";
-// import { PrestacionesCalc } from "@app/utils/PrestacionesCalc";
 
 export default function NominaMensual() {
-  // console.log(PrestacionesCalc(365));
+  const [loading, setLoading] = useState(null);
+
+  const handleBuscarClick = () => {
+    setLoading(true);
+
+    // Aquí iría tu lógica para buscar y cargar los datos
+
+    // Simulación de una llamada asincrónica
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  };
+
   return (
     <>
-      <Tabs variant="enclosed">
-        <TabList>
-          <Tab>Empleados</Tab>
-          <Tab>Empresa</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>
-            <NominaEmpleados />
-          </TabPanel>
-          <TabPanel>
-            <NominaEmpresa />
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
+      <Flex my={3} mx={4}>
+        <Text as="b" fontSize="md" w="100%" alignSelf="center">
+          Seleccione una fecha
+        </Text>
+        <Input type="date" mx={3} />
+        <Button colorScheme="teal" w="sm" mx={4} onClick={handleBuscarClick}>
+          Buscar
+        </Button>
+      </Flex>
+      {loading === true && (
+        <Flex justify="center" align="center" height="200px">
+          <Spinner size="xl" />
+        </Flex>
+      )}
+      {loading === false && (
+        <Tabs variant="enclosed">
+          <TabList>
+            <Tab>Empleados</Tab>
+            <Tab>Empresa</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <NominaEmpleados />
+            </TabPanel>
+            <TabPanel>
+              <NominaEmpresa />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      )}
     </>
   );
 }
