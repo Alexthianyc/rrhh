513fd46b14c0b6e938a140695da5fc7632c04279
Alexthian19 <ge19008@ues.edu.sv>
2023-06-16 08:41:51 -0600

    trabajadores edit campos fix

--- src/app/components/EditTrabajadores.jsx
+++ src/app/components/EditTrabajadores.jsx
@@ -114,7 +114,7 @@ export default function EditSeguimiento({ prevData }) {
                       onChange={handleInputChange}
                       type={col.typeCol}
                       defaultValue={
-                        prevData[col.key] != null && prevData[col.key]
+                        prevData[col.key] != null ? prevData[col.key] : ""
                       }
                       disabled={col.disabled}
                     />
--- src/app/components/NominaEmpleados.jsx
+++ src/app/components/NominaEmpleados.jsx
@@ -322,7 +322,7 @@ export default function NominaEmpleados({ esDiciembre, fechaNomina }) {
           window.print();
         }}
       >
-        Generar boletas de pago
+        Imprimir
       </Button>
     </>
   );
